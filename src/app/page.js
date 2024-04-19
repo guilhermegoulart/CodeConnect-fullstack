a061d47adac17747e01ef089905622a1d11d58f5
Guilherme Goulart Magela <guilhermegoulartmagela@gmail.com>
2024-04-19 11:56:32 -0300

    second fase of project

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,20 @@
 import { CardPost } from "@/components/CardPost"
 import logger from "@/logger"
-
 import styles from './page.module.css'
 import Link from "next/link"
+import db from "../../prisma/db"
 
 async function getAllPosts (page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
-  if (!response.ok) {
-    logger.error('Ops, alguma coisa correu mal')
-    return []
-  }
-  logger.info('Posts obtidos com sucesso')
-  return response.json()
+  try {
+
+    const posts = await db.post.findMany()
+
+    return { data: posts, prev: null, next: null}
+ 
+  } catch (error) {
+    logger.error('falha ao obter posts', { error })
+    return { data: [], prev: null, next: null }
+ }
 }
 
 export default async function Home({ searchParams }) {
