e8db3d8a700e1709260876d79d0a2b7fcd529bf9
Guilherme Goulart Magela <guilhermegoulartmagela@gmail.com>
2024-04-23 19:27:28 -0300

    modal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import styles from 'modal.module.css';
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+export const Modal = forwardRef(({ children }, ref) => {
+  const dialogRef = useRef(null);
+
+  const closeModal = () => {
+    dialogRef.current.close();
+  };
+
+  const openModal = () => {
+    dialogRef.current.showModal();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      openModal,
+      closeModal,
+    };
+  });
+
+  return (
+    <dialog className={styles.dialog} ref={dialogRef}>
+      <header className={styles.header}>
+        <button className={styles.button}>
+          x
+        </button>
+      </header>
+      {children}
+    </dialog>
+  );
+});
